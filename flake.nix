d288c53dd614eb6c298f99a2a8a05b9137d1d39c
Patrick <patrick@failmail.dev>
2025-02-04 22:50:54 +0100

    feat: lix-compat

--- flake.nix
+++ flake.nix
@@ -9,11 +9,6 @@
 
     flake-parts.url = "github:hercules-ci/flake-parts";
 
-    lib-net = {
-      url = "https://gist.github.com/duairc/5c9bb3c922e5d501a1edb9e7b3b845ba/archive/3885f7cd9ed0a746a9d675da6f265d41e9fd6704.tar.gz";
-      flake = false;
-    };
-
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     pre-commit-hooks = {
