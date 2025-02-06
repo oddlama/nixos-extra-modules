f1a3f0303b1fed8358026fbee4c71bdf61aa0f39
Patrick <patrick@failmail.dev>
2025-02-06 20:08:13 +0100

    feat: switch to flake-parts

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
 
     lib-net = {
       url = "https://gist.github.com/duairc/5c9bb3c922e5d501a1edb9e7b3b845ba/archive/3885f7cd9ed0a746a9d675da6f265d41e9fd6704.tar.gz";
@@ -22,63 +22,68 @@
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    devshell,
-    pre-commit-hooks,
-    ...
-  } @ inputs:
-    {
-      nixosModules.nixos-extra-modules = import ./modules;
-      nixosModules.default = self.nixosModules.nixos-extra-modules;
-      homeManagerModules.nixos-extra-modules = import ./hm-modules;
-      homeManagerModules.default = self.homeManagerModules.nixos-extra-modules;
-      overlays.nixos-extra-modules = import ./overlay.nix inputs;
-      overlays.default = self.overlays.nixos-extra-modules;
-    }
-    // flake-utils.lib.eachDefaultSystem (system: rec {
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [
-          devshell.overlays.default
-          self.overlays.default
-        ];
-      };
+  outputs =
+    { self, ... }@inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devshell.flakeModule
+        inputs.pre-commit-hooks.flakeModule
+      ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
 
-      # `nix flake check`
-      checks.pre-commit-hooks = pre-commit-hooks.lib.${system}.run {
-        src = nixpkgs.lib.cleanSource ./.;
-        hooks = {
-          alejandra.enable = true;
-          deadnix.enable = true;
-          statix.enable = true;
+      flake.modules = {
+        nixos = {
+          nixos-extra-modules = import ./modules;
+          default = self.modules.nixos.nixos-extra-modules;
+        };
+        home-manager = {
+          nixos-extra-modules = import ./hm-modules;
+          default = self.modules.home-manager.nixos-extra-modules;
         };
       };
-
-      # `nix fmt`
-      formatter = pkgs.alejandra;
-
-      # `nix develop`
-      devShells.default = pkgs.devshell.mkShell {
-        name = "nixos-extra-modules";
-        commands = with pkgs; [
-          {
-            package = alejandra;
-            help = "Format nix code";
-          }
-          {
-            package = statix;
-            help = "Lint nix code";
-          }
-          {
-            package = deadnix;
-            help = "Find unused expressions in nix code";
-          }
-        ];
-
-        devshell.startup.pre-commit.text = self.checks.${system}.pre-commit-hooks.shellHook;
+      flake.overlays = {
+        nixos-extra-modules = import ./overlay.nix inputs;
+        default = self.overlays.nixos-extra-modules;
       };
-    });
+
+      perSystem =
+        {
+          pkgs,
+          system,
+          config,
+          ...
+        }:
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+            overlays = [
+              self.overlays.default
+            ];
+          };
+          # `nix flake check`
+          pre-commit.settings.hooks = {
+            nixfmt-rfc-style.enable = true;
+            deadnix.enable = true;
+            statix.enable = true;
+          };
+          formatter = pkgs.nixfmt-rfc-style;
+          devshells.default = {
+            commands = with pkgs; [
+              {
+                package = statix;
+                help = "Lint nix code";
+              }
+              {
+                package = deadnix;
+                help = "Find unused expressions in nix code";
+              }
+            ];
+            devshell.startup.pre-commit.text = config.pre-commit.installationScript;
+          };
+        };
+    };
 }
